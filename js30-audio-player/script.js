8f2b789163ad8919658de9e508a22bdeefa1fdf1
Aliaksandr Pratasevich <urbankomp@gmail.com>
2023-09-24 14:46:03 +0300

    feat: add data music

--- js30-audio-player/script.js
+++ js30-audio-player/script.js
@@ -1,3 +1,66 @@
+const dataMusic = [
+    {
+        id: 1,
+        artist: "Linkin Park",
+        track: "In the End",
+        cover: "cover1",
+    },
+    {
+        id: 1,
+        artist: "Limp Bizkit",
+        track: "Break Stuff",
+        cover: "cover2",
+    },
+    {
+        id: 3,
+        artist: "Linkin Park",
+        track: "Numb",
+        cover: "cover3",
+    },
+    {
+        id: 4,
+        artist: "Limp Bizkit",
+        track: "Behind Blue Eyes",
+        cover: "cover4",
+    },
+    {
+        id: 5,
+        artist: "Linkin Park",
+        track: "A Light That Never",
+        cover: "cover5",
+    },
+    {
+        id: 6,
+        artist: "Limp Bizkit",
+        track: "Red Light - Green Light",
+        cover: "cover6",
+    },
+    {
+        id: 7,
+        artist: "Linkin Park",
+        track: "What I've Done",
+        cover: "cover7",
+    },
+    {
+        id: 8,
+        artist: "Limp Bizkit",
+        track: "Getcha Groove On",
+        cover: "cover8",
+    },
+    {
+        id: 9,
+        artist: "Linkin Park",
+        track: "The Catalyst",
+        cover: "cover9",
+    },
+    {
+        id: 10,
+        artist: "Linkin Park",
+        track: "Talking to Myself",
+        cover: "cover10",
+    }
+]
+
 const player = document.getElementById('player');
 const audio = document.getElementById('audio');
 const coverImage = document.getElementById('cover-img');
@@ -13,25 +76,14 @@ const nameSong = document.getElementById('song-name');
 const timeCurrent = document.getElementById('time-current');
 const timeTotal = document.getElementById('time-total');
 
-const dataNameArtist = ['Linkin Park',
-                    'Limp Bizkit',
-                    'Linkin Park',
-                    'Limp Bizkit',
-                    'Linkin Park',
-                    'Limp Bizkit',
-                    'Linkin Park',
-                    'Limp Bizkit',
-                    'Linkin Park',
-                    'Linkin Park'];
 
-const dataNameSong = ['In the End',
-                    'Break Stuff',
-                    'Numb',
-                    'Behind Blue Eyes',
-                    'A Light That Never',
-                    'Red Light - Green Light',
-                    "What I've Done",
-                    'Getcha Groove On',
-                    'The Catalyst',
-                    'Talking to Myself'];
+let indexSongs = 0;
 
+function loadingSongs(song) {
+    audio.src = `assets/music/${song.track}.mp3`;
+    coverImage.src = `assets/image_album/${song.cover}.jpeg`;
+    nameArtist.innerHTML = song.artist;
+    nameSong.innerHTML = song.track;
+}
+
+loadingSongs(dataMusic[indexSongs]);
