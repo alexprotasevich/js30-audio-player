2a94dc9f027f66bf8a2f143b1a658f4dd424175f
Aliaksandr Pratasevich <urbankomp@gmail.com>
2023-09-24 13:31:39 +0300

    feat: id and const js

--- js30-audio-player/script.js
+++ js30-audio-player/script.js
@@ -1,1 +1,37 @@
-// js
+const player = document.getElementById('player');
+const audio = document.getElementById('audio');
+const coverImage = document.getElementById('cover-img');
+const buttonPlay = document.getElementById('btn-play');
+const playOrPause = document.getElementById('play-or-pause');
+const buttonPrevious = document.getElementById('btn-prev');
+const buttonNext = document.getElementById('btn-next');
+const progressBar = document.getElementById('progress-bar');
+const progressLine = document.getElementById('progress-line');
+const nameArtist = document.getElementById('song-artist');
+const nameSong = document.getElementById('song-name');
+
+const timeCurrent = document.getElementById('time-current');
+const timeTotal = document.getElementById('time-total');
+
+const dataNameArtist = ['Linkin Park',
+                    'Limp Bizkit',
+                    'Linkin Park',
+                    'Limp Bizkit',
+                    'Linkin Park',
+                    'Limp Bizkit',
+                    'Linkin Park',
+                    'Limp Bizkit',
+                    'Linkin Park',
+                    'Linkin Park'];
+
+const dataNameSong = ['In the End',
+                    'Break Stuff',
+                    'Numb',
+                    'Behind Blue Eyes',
+                    'A Light That Never',
+                    'Red Light - Green Light',
+                    "What I've Done",
+                    'Getcha Groove On',
+                    'The Catalyst',
+                    'Talking to Myself'];
+
